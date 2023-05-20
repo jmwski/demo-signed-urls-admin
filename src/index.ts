90354510dff1c4b34d005e7026fa89bcc7e99518
Jan Wyszynski <wyszynski@google.com>
2023-05-19 20:01:58 -0400

    signed urls

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getStorage } from 'firebase-admin/storage';
-import { DownloadResponse } from '@google-cloud/storage';
+// import { GetSignedUrlConfig, GetSignedUrlResponse} from '@google-cloud/storage';
 
 const config = {
   projectId: "demo-signed-urls",
-  credential: cert(require('../key.json')),
-  storageBucket: "demo-signed-urls.appspot.com",
+  credential: cert(require("../key.json"))
 };
 
 process.env["FIREBASE_STORAGE_EMULATOR_HOST"] = "127.0.0.1:9199"
@@ -15,11 +14,12 @@ const storage = getStorage(app);
 
 (async function() {
   try {
-    const bucket = storage.bucket()
-    const f: DownloadResponse = await bucket.file("c").download()
-    console.log(f[0].toString("utf-8"))
+    await storage.bucket("demo-signed-urls.appspot.com").file("c").getMetadata()
     // The emulator doesn't support this.
-    // await bucket.getSignedUrl({ expires: 604800 } as GetBucketSignedUrlConfig)
+    // const expires = new Date()
+    // expires.setTime(expires.getTime() + (1 * 60 * 60 * 1000))
+    // const url: GetSignedUrlResponse = await storage.bucket("demo-signed-urls.appspot.com").file("c").getSignedUrl({ action: "read", expires } as GetSignedUrlConfig)
+    // console.log(url[0])
   } catch (e) {
     console.log(`ERROR: ${e}`);
   }
